7c6b2ed71b0c4ea6bac1109d8f4866f76204e70e
Karin Ljunggren <kaljunggren@gmail.com>
2018-11-01 22:07:49 +0100

    home changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,9 @@ const Home = () => {
                     <div className="card">
                         <div className="card-body">
                             <p>Welcome to the <strong>Boozang Test Lab</strong>!</p>
-                            <p> Here you can test different aspects of web applications.
-                            It's ideal to practice how to do test automation, and it also to acts as a testing ground for your automation tools.</p>
-                            <p>The site is divided into a number of test categories, that each has a number of problems to solve. Each problem has a help text on how this is solved using the Boozang tool, and a video on how to solve it. </p>
+                            <p>Test different aspects of web applications and practice how to do test automation.</p>
+                            <p>The site also to acts as a testing ground for your automation tools.</p>
+                            <p>There are a number of test categories, that each has a number of problems to solve. Each problem has a help text on how this is solved using the Boozang tool, and a video on how to solve it. </p>
                             <p><a href="https://boozang.com/">Boozang</a> also gives a free
                             <a href=""> Udemy course</a> that takes you through each task with solution.</p>
 
