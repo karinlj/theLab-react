ff64335082ab409d6b7aa8b94ad28c6d97444193
Mats Ljunggren <mljunggren@gmail.com>
2018-11-05 15:02:22 -0500

    Changed all info boxes

--- src/components/Timing/Speed.js
+++ src/components/Timing/Speed.js
@@ -43,9 +43,9 @@ class Speed extends Component { //class based
                     <div className="timing-section">
                         <header>
                             <h1 className="">Speed</h1>
-                            <p>“Wait and click as fast as you can”: Show a button that starts the game “Start Game”. </p>
-                            <p> Wait random rt (0s &lt;  rt &lt;  60s) with displaying another button “End Game”.</p>
-                            <p> When End Game is clicked show “Success” and separate score of (t-rt), which i how long it took to hit button</p>
+                            <p>This game tests the user reaction time. This is also useful to show-case test automation render waits. </p>
+                            <p> The game start with the user hitting "Start Game" button. Another button "End Game" will appear after x seconds,</p>
+                            <p> where x is random time between 1 and 10 seconds. The smaller the number of milliseconds, the better. </p>
                         </header>
 
                         <MainBtn classProp="button green-btn" titleProp="Start" clickProp={this.handleStart} />
@@ -56,7 +56,7 @@ class Speed extends Component { //class based
                 </div>
 
                 <div className="col-12 col-md-4">
-                    <Sidebar heading="Why learn: Speed?" text="A classic problem in test automation is render waits. If certain elements are taking long to render, the test might fail. On the otherhand, if we create lomg delays the overall test takes too long to run. Best if is we can wait until an eleemnt appears, but then click it as fast as we can.  " />
+                    <Sidebar heading="Why learn: Speed?" text="A classic problem in test automation is render waits. If certain elements are taking long to render, the test might fail. On the other hand, if we create long delays the overall test takes too long to run. Best if is we can wait until an element appears, but then click it as fast as we can.  " />
 
                 </div>
 
