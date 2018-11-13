4f49f3b0a0a3634acfa9283d9e05775811e688da
Karin Ljunggren <kaljunggren@gmail.com>
2018-11-13 16:45:50 +0100

    home text

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                             <p>Welcome to the <strong>Boozang Test Lab</strong>!</p>
                             <p>Test different aspects of web applications and practice how to do test automation.</p>
                             <p>The site also to acts as a testing ground for your automation tools.</p>
-                            <p>There are a number of test categories, that each has a number of problems to solve. Each problem has a help text on how this is solved using the Boozang tool, and a video on how to solve it. </p>
+                            <p>Here you find a number of test categories, that each has a number of problems to solve. Each problem has a help text on how this is solved using the Boozang tool, and a video on how to solve it. </p>
                             <p><a href="https://boozang.com/">Boozang</a> also gives a free
                             <a href="https://www.udemy.com/code-less-test-automation-with-boozang/"> Udemy course</a> that takes you through each task with solution.</p>
 
